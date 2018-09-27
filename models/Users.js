3efa3e28c5f4e9e3f0ef6dbcfff377edde8b7f30
Carfilaf Nuñez <esteban.carfilaf@sonda.com>
2018-09-27 17:53:27 -0300

    Ok

--- models/Users.js
+++ models/Users.js
@@ -3,19 +3,22 @@ var db=require('../dbconnection'); //reference of dbconnection.js
 var User={
 
 	getAllUsers:function(callback){
-		return db.query("Select * from User",callback);
+		return db.query("Select * from Usuarios",callback);
 	},
 	getUserById:function(id,callback){
-		return db.query("select * from User where Id=?",[id],callback);
+		return db.query("select * from Usuarios where Id=?",[id],callback);
+	},
+	getUserByUser:function(usuario,callback){
+		return db.query("select * from Usuarios where Usuario=?",[usuario],callback);
 	},
 	addUser:function(User,callback){
-		return db.query("Insert into User values(?,?,?)",[User.Id,User.Title,User.Status],callback);
+		return db.query("Insert into Usuarios values(?,?,?,?,?,?)",[User.Id,User.Nombre,User.Apellido,User.Email,User.Usuario,User.Clave],callback);
 	},
 	deleteUser:function(id,callback){
-		return db.query("delete from User where Id=?",[id],callback);
+		return db.query("delete from Usuarios where Id=?",[id],callback);
 	},
 	updateUser:function(id,User,callback){
-		return db.query("update User set Title=?,Status=? where Id=?",[User.Title,User.Status,id],callback);
+		return db.query("update Usuarios set Nombre=?,Apellido=? where Id=?",[User.Nombre,User.Apellido,id],callback);
 	}
 };
 module.exports=User;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,68 @@
 var express = require('express');
 var router = express.Router();
+var User=require('../models/Users');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});*/
+
+router.get('/:id?',function(req,res,next){
+	if(req.params.id){
+		User.getUserById(req.params.id,function(err,rows){
+			if(err){
+				res.json(err);
+			}
+			else{
+				res.json(rows);
+			}
+		});
+	}
+	else{
+		User.getAllUsers(function(err,rows){
+			if(err){
+				res.json(err);
+			}
+			else{
+				res.json(rows);
+			}
+		});
+	}
+});
+
+router.get('/:usuario?',function(req,res,next){
+	if(req.params.usuario){
+		User.getUserByUser(req.params.usuario,function(err,rows){
+			if(err){
+				res.json(err);
+			}
+			else{
+				res.json(rows);
+			}
+		});
+	}
+});
+
+router.post('/',function(req,res,next){
+	User.addUser(req.body,function(err,count){
+		if(err){
+			res.json(err);
+		}
+		else{
+			res.json(req.body);
+		}
+	});
+});
+
+router.put('/:id',function(req,res,next){
+	User.updateUser(req.params.id,req.body,function(err,rows){
+		if(err){
+			res.json(err);
+		}
+		else{
+			res.json(rows);
+		}
+	});
 });
 
 module.exports = router;
